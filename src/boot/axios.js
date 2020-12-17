517aed8c8c9b3b9ed1866927c3b64c28f63b83c3
LongchuanYu <remly@qq.com>
2020-12-17 09:36:37 +0800

    add debug flag

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -1,5 +1,12 @@
 import Vue from 'vue'
 import axios from 'axios'
-axios.defaults.baseURL = 'http://127.0.0.1:5000/'
+let debug = false;
+
+
+if(debug){
+	axios.defaults.baseURL = 'http://127.0.0.1:5000/'
+}else{
+	axios.defaults.baseURL = 'http://49.235.109.170:5000/'
+}
 axios.defaults.timeout = 5000
 Vue.prototype.$axios = axios
